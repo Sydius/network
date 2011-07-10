1f292fd4ed727a73908e9ad0fbf63ae3e1e2c893
Christopher Ogden <sydius@gmail.com>
2011-07-10 01:52:32 -0700

    Put client code in server.

--- server.cpp
+++ server.cpp
@@ -35,12 +35,39 @@ class Server
 
 int main(int argc, char * argv[])
 {
+    bool runServer = false;
+    bool connectToServer = false;
+
+    switch (atoi(argv[1])) {
+        case 0:
+            runServer = true;
+            break;
+        case 1:
+            connectToServer = true;
+            break;
+        case 2:
+            runServer = true;
+            connectToServer = true;
+            break;
+    }
+
     try {
         Connection::IOService ioService;
-        Server server(ioService, RPCMethods());
+        std::shared_ptr<Server> server;
+
+        if (runServer) {
+            server = std::shared_ptr<Server>{new Server(ioService, RPCMethods())};
+        }
+
+        if (connectToServer) {
+            Connection::pointer connection = Connection::connect(ioService, RPCMethods(), "localhost", "2000");
+            connection->execute(CLIENT_RPC(foo), 5);
+        }
+
         ioService.run();
-    } catch (std::exception & e) {
+    } catch (const std::exception & e) {
         std::cerr << e.what() << std::endl;
     }
+
     return 0;
 }
--- wscript
+++ wscript
@@ -12,13 +12,6 @@ def configure(conf):
 
 def build(bld):
     bld.program(
-        source=['client.cpp', 'shared.cpp'],
-        includes=['../call-with-tuple', '../serialize-tuple', '../dynamic-invocation'],
-        target='client',
-        lib=['boost_system-mt', 'boost_serialization'],
-        cxxflags='-O3 --std=c++0x --pedantic -Wall'
-    )
-    bld.program(
         source=['server.cpp', 'shared.cpp'],
         includes=['../call-with-tuple', '../serialize-tuple', '../dynamic-invocation'],
         target='server',
